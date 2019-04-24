d24bd9613950bd841a60e836da3224dd5db22f72
mridulganga <mridul.kepler@gmail.com>
2019-04-24 11:39:15 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+import requests
+
+reader = SimpleMFRC522()
+
+
+def detected(id):
+    body={
+        "id":id
+    }
+    r = requests.post("https://easykey-client.herokuapp.com/rfid", 
+                json=body, 
+                headers={"content-type":"application/json"})
+
+try:
+    while(True):
+            id, text = reader.read()
+            print(id)
+            print(text)
+            detected(id)
+finally:
+    GPIO.cleanup()
