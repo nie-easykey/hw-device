b473856c6db0ae6eae805ecd75f781492c5d7ef5
mridulganga <mridul.kepler@gmail.com>
2019-05-09 19:19:29 +0530

    adding ajax rfid handler and interval

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
+
+rfid = "3000"
 
 @app.route('/')
 def hello_world():
     return render_template("index.html")
+
+@app.route("/rfid", methods=["GET","POST"])
+def rfid_handler():
+    global rfid
+    if request.method == "GET":
+        return rfid
+    elif request.method == "POST":
+        rfid = request.get_json()["rfid"]
 
 
 app.run(host="0.0.0.0",port=5000, debug=True)
